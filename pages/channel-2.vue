70a3843a96ff560d4c443224c88943c71a666bf8
Nick <nick@rosielee.co.uk>
2021-05-04 09:35:13 +0100

    add pages

---
+++ pages/channel-2.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h1>Channel 2</h1>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/radio.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h1>Radio</h1>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
