a0577175f0f612f941ffdd2e512d1bfbf4a884d9
Nick <nick@rosielee.co.uk>
2021-07-29 19:48:53 +0100

    add channel 2 widgets

--- pages/channel-2.vue
+++ pages/channel-2.vue
@@ -9,7 +9,10 @@
     <article>
       <aaja-container class="channel2__content">
         <div class="iframe-wrap">
-          <iframe
+          <iframe id="embed_player" frameborder="0" width="350" height="396" src="https://aaja2.airtime.pro/embed/player?stream=auto&skin=2"></iframe>
+          <iframe class="schedule-widget" height="400px" width="550px" scrolling="yes" frameborder="0" src=https://aaja2.airtime.pro/embed/weekly-program></iframe>
+
+          <!-- <iframe
             width="560"
             height="315"
             :src="youtubeLink"
@@ -17,7 +20,7 @@
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
-          ></iframe>
+          ></iframe> -->
         </div>
       </aaja-container>
     </article>
@@ -53,23 +56,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
+  width: 100%;
+  overflow: scroll;
+}
 .channel2__ {
   &content {
     .iframe-wrap {
       width: 100%;
-      height: 0;
+      height: auto;
       position: relative;
-      padding-bottom: 56.25%;
-      background-color: var(--dark);
-      background-image: url('https://res.cloudinary.com/nickjohn/image/upload/f_auto,w_500/v1620549353/Aaja/aaja_black_logo.png');
-      background-size: 40%;
+      // padding-bottom: 56.25%;
+      // background-color: var(--dark);
+      // background-image: url('https://res.cloudinary.com/nickjohn/image/upload/f_auto,w_500/v1620549353/Aaja/aaja_black_logo.png');
+      // background-size: 40%;
     }
     iframe {
       width: 100%;
-      height: 100%;
-      position: absolute;
+      // height: 100%;
+      position: relative;
       top: 0;
       left: 0;
+    }
+    .schedule-widget {
+      width: 100%;
     }
   }
 }
