f7948be46fe5fe55f97c541813d20162519ff3e9
Xavier Defontaine <xdefontaine@gmail.com>
2024-05-24 16:07:53 +0100

    feat: add photos to studio pages

--- cms/schemas/studioPage.js
+++ cms/schemas/studioPage.js
@@ -19,9 +19,34 @@ export default {
       type: 'image'
     },
     {
-      title: 'Studio Booking Widget header',
+      title: 'Studio Bookings header',
       name: 'subHeading',
       type: 'blockContent',
+    },
+    {
+      title: 'Studio Images',
+      description: 'Drag and drop images from folder to add several at once.',
+      name: 'images',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          options: {
+            hotspot: true, // <-- Defaults to false
+          },
+          fields: [
+            {
+              name: 'altText',
+              type: 'string',
+              title: 'Alt Text',
+              options: {
+                isHighlighted: true, // <-- make this field easily accessible
+              },
+            },
+          ],
+        },
+      ],
+      // validation: (Rule) => Rule.min(3).max(3),
     },
   ],
 }
